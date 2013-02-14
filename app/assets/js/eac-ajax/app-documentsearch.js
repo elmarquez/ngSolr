5d2c43c4013fa16276dc9a28ed3d7d040f4777b1
Davis Marques <davis.marques@unimelb.edu.au>
2013-02-14 16:36:57 +1100

    Modified location search page to match document/image search, minor changes to application organization

--- app/assets/js/eac-ajax/app-documentsearch.js
+++ app/assets/js/eac-ajax/app-documentsearch.js
@@ -7,7 +7,7 @@
 /*---------------------------------------------------------------------------*/
 /* Application                                                               */
 
-var app = angular.module('eacajax', ['Directives','Filters','MapServices','SearchServices']);
+var app = angular.module('eacajax', ['Directives','Filters','SearchServices']);
 
 // @see https://groups.google.com/forum/#!msg/angular/FUPnNj7CwhY/_U1S7PpvCtcJ
 // $locationProvider.html5Mode(false).hashPrefix('');
